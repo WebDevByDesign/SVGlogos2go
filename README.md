9c6a5ecd462a5a8d7e6897af380f7f9a45d2c373
Rachael <124227903+missblasko@users.noreply.github.com>
2023-07-27 13:34:44 -0500

    pushed updates and format

--- README.md
+++ README.md
@@ -3,21 +3,20 @@
 
 ![Visual Studio Code](https://img.shields.io/badge/--007ACC?logo=visual%20studio%20code&logoColor=ffffff)
 
-Watch the demo here: [https://you]
+Watch the demo here: [https://youtu.be/HtrjOfTqCtk]
 ## Description
-  After answering a small series of questions., this SVG logo creator builds a basic logo for the User.
+  After answering a small series of questions, this simple SVG logo creator builds a basic logo for the User.
 ## Table of Contents
 1. [Usage](#usage)
 2. [Installation](#installation)
 3. [Test](#test)
 4. [Contribution](#contribution)
-5. [License](#license)
 ## Usage
-  Once all packages have been downloaded (see install instructions), In your terminal, type "npm start", to initialize the SVG logo creator. Answer questions based on your preference.
+  In a time crunch and need to whip up a quick and simple logo? Look no further than SVGlogos2go!
 ## Installation
-  First, ensure you are in the integrated terminal of the repo project you would like to create the SVG logo for. Second, clone the SVG-Logos-to-go repository onto your local drive. Next, on the command line, type "npm i", to install the required packages. Once these packages are installed, type ```npm start``` on the command line and follow the prompts.
+  First, ensure you are in the integrated terminal of the repo project you would like to create the SVG logo for. Second, clone the SVGlogos2go repository onto your local drive. Next, on the command line, type "npm i", to install the required packages. Once these packages are installed, type ```npm start``` on the command line and follow the prompts.
 ## Test
-  In your terminal, type ```npm test``` and answer the prompts. Once complete, a logo.SVG will appear in your desired file location and now you have a simple logo- to go!
+  In your terminal, type ```npm test``` and answer the prompts. Once complete, a logo.SVG will appear in your desired file location and now you have a simple SVGlogo-2-go!
 
 Questions? Contact me:
 [GitHub](https://github.com/missblasko) |
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-const inquirer = require('inquirer');
-const fs = require('fs');
-const shapes = require('./lib/shapes.js');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const shapes = require("./lib/shapes.js");
 
 function generateSvg(res) {
   const shape = res.shapeChoice;
@@ -15,43 +14,37 @@ function generateSvg(res) {
   // Generate SVG based on the shape and other properties
   let svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
 
-  
-
-  if (shape === 'Circle') {
+  if (shape === "Circle") {
     // Generate SVG for a circle shape w/ <g> tag
     svg += `<g transform="translate(${centerX}, ${centerY})">`;
-    svg+= `<circle cx="0" cy="0" r="40" fill="${shapeColor}" />`;
+    svg += `<circle cx="0" cy="0" r="40" fill="${shapeColor}" />`;
     // Center text in circle shape
     svg += `<text x="0" y="0" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${logoText}</text>`;
     svg += `</g>`;
-
- 
-
-  } else if (shape === 'Triangle') {
+  } else if (shape === "Triangle") {
     //Centertext on SVG logo for Triangle
-       const triangleCenterX = (50 + 90 + 10) / 3;
-       const triangleCenterY = (5 + 90 + 90) / 3;
+    const triangleCenterX = (50 + 90 + 10) / 3;
+    const triangleCenterY = (5 + 90 + 90) / 3;
     // Generate SVG for a triangle shape w/ <g> tag
     svg += `<g transform="translate(${centerX}, ${centerY})">`;
     svg += `<polygon points="50,5 90,90 10,90" fill="${shapeColor}" />`;
     // Center text in selected shape
     svg += `<text x="${triangleCenterX}" y="${triangleCenterY}" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${logoText}</text>`;
     svg += `</g>`;
-    
-  } else if (shape === 'Square') {
+  } else if (shape === "Square") {
     //Centertext on SVG logo for Triangle
     const squareCenterX = (50 + 90 + 10) / 3;
     const squareCenterY = (5 + 90 + 90) / 3;
     // Generate SVG for a square shape w/ <g> tag
     svg += `<g transform="translate(${centerX}, ${centerY})">`;
     svg += `<rect x="10" y="10" width="80" height="80" fill="${shapeColor}" />`;
-   // Center text in selected shape
-   svg += `<text x="${squareCenterX}" y="${squareCenterY}" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${logoText}</text>`;
-   svg += `</g>`;
+    // Center text in selected shape
+    svg += `<text x="${squareCenterX}" y="${squareCenterY}" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${logoText}</text>`;
+    svg += `</g>`;
   }
-   // Close SVG tag
+  // Close SVG tag
   svg += `</svg>`;
-  
+
   return svg;
 }
 
@@ -68,49 +61,48 @@ function writeToFile(fileName, data) {
   });
 }
 
-  const shapeChoice = [ 
-    { name: "Circle", value: "Circle" },
-    { name: "Triangle", value: "Triangle" },
-    { name: "Square", value: "Square" },
-  ];
-    
+const shapeChoice = [
+  { name: "Circle", value: "Circle" },
+  { name: "Triangle", value: "Triangle" },
+  { name: "Square", value: "Square" },
+];
 
 function promptQuestions() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message:
-            "What text (three characters max, ie 'RJB') would you like your logo to display? ",
-          name: "logoText",
-        },
-        {
-          type: "input",
-          message:
-            "What color would you like this text to be? (Type out color or choose a hexadecimal number)",
-          name: "textColor",
-        },
-        {
-          type: "list",
-          message: "What shape would you like your logo to display?",
-          choices: shapeChoice,
-          name: "shapeChoice",
-        },
-        {
-          type: "input",
-          message:
-            "Choose your shape's color (Type out color or choose a hexadecimal number)",
-          name: "shapeColor",
-        },
-      ])
-      .then((answers) => {
-        if (answers.logoText.length > 3) {
-          console.log("Must enter a value (3 characters max)");
-          promptQuestions();
-        } else {
-          writeToFile('logo.svg', answers);
-        }
-      });
-    }
-   
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What text (three characters max, ie 'RJB') would you like your logo to display? ",
+        name: "logoText",
+      },
+      {
+        type: "input",
+        message:
+          "What color would you like this text to be? (Type out color or choose a hexadecimal number)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape would you like your logo to display?",
+        choices: shapeChoice,
+        name: "shapeChoice",
+      },
+      {
+        type: "input",
+        message:
+          "Choose your shape's color (Type out color or choose a hexadecimal number)",
+        name: "shapeColor",
+      },
+    ])
+    .then((answers) => {
+      if (answers.logoText.length > 3) {
+        console.log("Must enter a value (3 characters max)");
+        promptQuestions();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+
 promptQuestions();
